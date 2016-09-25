811238ceafc0ddf57925bb84136e99912a64eead
emteg <emteg@mailbox.org>
2016-09-25 15:12:03 +0200

    Config template filled with explainatory values

--- config.template.php
+++ config.template.php
@@ -4,10 +4,10 @@ $config["datenbankBenutzer"] = "user";
 $config["datenbankPasswort"] = "password";
 $config["datenbankName"] = "infobeamer";
 $config["rootDir"] = "beamercontrol/";
-$config["beamerDirAbs"] = "";
+$config["beamerDirAbs"] = "/absolute/path/to/beamer/";
 $config["beamerDir"] = "beamer/";
-$config["beamerModulePfad"] = "";
-$config["beamerDesignsPfad"] = "";
-$config["beamerBilderPfad"] = "";
+$config["beamerModulePfad"] = "/absolute/path/to/beamer/module/";
+$config["beamerDesignsPfad"] = "/absolute/path/to/beamer/designs/";
+$config["beamerBilderPfad"] = "/absolute/path/to/beamer/img_upload/";
 $config["beamerBilderPfadRelativ"] = "../beamer/img_upload/";
 ?>
